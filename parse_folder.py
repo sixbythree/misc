a63817444941b7890fc8dc1840f2a8d8f7385261
Sammy <34468480+soge26@users.noreply.github.com>
2023-01-26 07:45:05 -0500

    working code for copying files to any destination

--- parse_folder.py
+++ parse_folder.py
@@ -11,29 +11,53 @@ import webbrowser
 import subprocess as sp
 import webbrowser as wb
 # import requests
-orig_dir = os.getcwd()
-phq = '/Users/sammyoge/photo_hq/'
-folder_path = '/Volumes/NIKON D850 /DCIM/115ND850'
 
-config = configparser.ConfigParser()
-config.read(phq + 'phq.cfg')
+# Paths
+# A. parse_folder folder path
+# B. photo_hq folder path
+# C. sd folder path
+
+orig_dir = os.getcwd()  # A.
+phq = '/Users/sammyoge/photo_hq/'  # B.
+folder_path = '/Volumes/NIKON D850 /DCIM/115ND850'  # C.
+
+# Config
+# A. creating global config object
+# B. reading phq.cfg file
+
+config = configparser.ConfigParser()  # A.
+config.read(phq + 'phq.cfg')  # B.
 
 
-def client(ctype=None):
-    config = configparser.ConfigParser()
+def client(ctype=None, c=False):
+    """
+
+    :param reset (False): Boolean to reset cwd to original directory (orig_dir).
+    :return:
+
+
+    Description:
+
+    A. creating local config object.
+    B. reading phq.cfg file into config object.
+    """
+
+    config = configparser.ConfigParser()  # A.
     config.read(phq + 'phq.cfg')
 
-    if ctype == 'sd':
-        folder = input("""Enter desired date for file copying from the available dates listed below:
+    if c==True:
+        folder = ctype or input("""Enter desired subfolder for file copying from the available folders listed below:
                           \n{} \n>""".format((sp.run('ls', cwd=config['PATHS']['sd_path'],shell=True
                                                      ,capture_output=True,text=True).stdout))) + '/'
         return config['PATHS']['sd_path'] + folder
-    elif ctype == 'x3':
+
+
+    if ctype == 'x3':
         return config['PATHS']['bythree_dest_path']
     elif ctype == 'rwci':
         return config['PATHS']['rwci_dest_path']
     else:
-        return input('Enter desired directory:') or '/Users/sammyoge/photo_hq/'
+        return input('Enter desired directory for writing files to:') or '/Users/sammyoge/photo_hq/'
 
 def read_files(path=None):
     """
@@ -45,9 +69,11 @@ def read_files(path=None):
     folder = path or orig_dir
 
     data = sp.run("ls | xargs stat -f '%SB %N' -t'%m-%d-%y'",
-                  cwd=folder, shell=True,capture_output=True, text=True).stdout
-    d = list(filter(len,data.split('\n')))
-
+                  cwd=folder, shell=True, capture_output=True, text=True).stdout
+    d = list(filter(None,data.split('\n')))
+        # The first parameter is a function which has a condition to filter the input.
+        # It returns True on success or False otherwise. However, if you provide a None,
+        # then it removes all items except those evaluate to True.
     return d
 
 def files_dict(data):
@@ -61,9 +87,16 @@ def files_dict(data):
 
     files = {}
     key_func = lambda x: x[0:8]
+    # This function takes the first 8 charachters of each file string
 
     for key, value in itertools.groupby(data,key_func):
+        # itertools groups the data by the key_function. So in this case, the data will be grouped
+        # by their first 8 characters where in this case is the date. The keys will be the date and
+        # the values will be each file that has that same date.
+
         files[key] = [i.replace('{}'.format(key),'').lstrip(' ') for i in list(value)]
+        # files now becomes a dictionary that who's keys are the dates of the files and the values are
+        # the files themselves of that key date.
 
     return files
 
@@ -85,16 +118,22 @@ def run(t=None,f=None):
     """
     :param data (list): List of files with date information.
     :return:
+
+    Description:
+
+    A. recording user input for source directory in fr
+    B. recording user input for destination directory in to
+    C. storing file path of src directory for copying files in fr
+    D. storing file path of destination directory for copying files in to
     """
 
-    fr = f or input('Enter source directory:')
-    to = t or input('Enter destination directory:')
+    # fr = f or input('Enter source directory:')  # A.
+    # to = t or input('Enter destination directory:')  # B.
 
-    src = client(fr)
+    src = client(ctype=f, c=True)
+    dest = client(ctype=t)
+
     print('Reading contents of %s' % src)
-
-    dest = client(to)
-
     data = read_files(path=src)
     content = files_dict(data)
     catalog = select_dates(content)
@@ -104,11 +143,11 @@ def run(t=None,f=None):
     print('Writing contents of %s to %s' % (src, dest))
     cmd1 = 'mkdir %s' % folders
     sp.run(cmd1, cwd=dest, shell=True)
-    print(content)
-    #cmd1 = 'mkdir {%s}' % ','.join(list(catalog.keys()))
+    # print(content)
+    # cmd1 = 'mkdir {%s}' % ','.join(list(catalog.keys()))
 
     cp_cmd = []
-    cp_cmd += ['cp {f} {d}'.format(f=form(content[k]), d=dest + k) for k in catalog]
+    cp_cmd += ['cp {f} "{d}"'.format(f=form(content[k]), d=dest + k) for k in catalog]
     print(cp_cmd)
     [sp.run(cmd,cwd=src,shell=True) for cmd in cp_cmd]
 
